13c813bfd0b0f5ae356df7c5af9a77a0cf068ee3
bryahchin.mihail <bryahchin.mihail@otr.ru>
2022-01-06 15:23:32 +0300

    Initial commit

---
+++ src/main/java/com/java/teaching/blog/JavaBlogApp.java
@@ -0,0 +1,13 @@
+package com.java.teaching.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBlogApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBlogApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/teaching/blog/JavaBlogAppTests.java
@@ -0,0 +1,13 @@
+package com.java.teaching.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBlogAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
