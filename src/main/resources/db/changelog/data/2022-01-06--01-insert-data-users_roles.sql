ecf8987beb325cedaf4a691747acc011acec8ae6
bryahchin.mihail <bryahchin.mihail@otr.ru>
2022-02-13 19:21:16 +0300

    docker back

--- src/main/resources/db/changelog/data/2022-01-06--01-insert-data-users_roles.sql
+++ src/main/resources/db/changelog/data/2022-01-06--01-insert-data-users_roles.sql
@@ -1,8 +1,9 @@
 --liquibase formatted sql
 
 --changeset mihail:2022-01-06--01-insert-data-users_roles
-insert into users (username, password, email) values ('admin', 'password', 'admin@mail.ru'),
+
+INSERT INTO USERS (USERNAME, PASSWORD, EMAIL) VALUES ('admin', 'password', 'admin@mail.ru'),
 ('misha','123', 'br.mihail@yandex.ru'),
 ('ivanov','abc', 'ivanov@google.com');
-insert into roles(`name`) values ('ROLE_USER'), ('ROLE_ADMIN');
-insert into users_roles (user_id, roles_id) values (1, 2), (2, 1), (3, 1);
+INSERT INTO ROLES(`NAME`) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+INSERT INTO USERS_ROLES (USER_ID, ROLES_ID) VALUES (1, 2), (2, 1), (3, 1);
--- src/main/resources/db/changelog/data/2022-01-07--03-insert-data-comments.sql
+++ src/main/resources/db/changelog/data/2022-01-07--03-insert-data-comments.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset mihail:2022-01-06--03-insert-data-comments
-insert into comments (message, date_message, id_post, id_user) values ('Отличная статья, спасибо!' , '2021-10-06 ', 1, 1),
+INSERT INTO COMMENTS (MESSAGE, DATE_MESSAGE, ID_POST, ID_USER) VALUES ('Отличная статья, спасибо!' , '2021-10-06 ', 1, 1),
 ('Машинный перевод. Смысл очень сложно уловить, хоть отредактировали бы, перед выкладыванием' , '2022-01-01', 2, 1),
 ('Если честно, странное решение.
 Если из всего ответа нужно только два поля, логично было получить только их.
