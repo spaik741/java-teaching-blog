6fca39d003929850b3f6324f47af6394bac8953e
bryahchin.mihail <bryahchin.mihail@otr.ru>
2022-02-18 22:02:59 +0300

    docker back on postgres фронт локально собирается, бек докером

--- src/main/resources/db/changelog/1.0/2022-01-06--01-create-roles-table.sql
+++ src/main/resources/db/changelog/1.0/2022-01-06--01-create-roles-table.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset mihail:2022-01-06--01-create-roles-table
-CREATE TABLE ROLES(ID BIGINT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE ROLES(ID SERIAL UNIQUE,
  NAME VARCHAR(30));
--- src/main/resources/db/changelog/data/2022-01-06--01-insert-data-users_roles.sql
+++ src/main/resources/db/changelog/data/2022-01-06--01-insert-data-users_roles.sql
@@ -5,5 +5,5 @@
 INSERT INTO USERS (USERNAME, PASSWORD, EMAIL) VALUES ('admin', 'password', 'admin@mail.ru'),
 ('misha','123', 'br.mihail@yandex.ru'),
 ('ivanov','abc', 'ivanov@google.com');
-INSERT INTO ROLES(`NAME`) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
+INSERT INTO ROLES(NAME) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
 INSERT INTO USERS_ROLES (USER_ID, ROLES_ID) VALUES (1, 2), (2, 1), (3, 1);
